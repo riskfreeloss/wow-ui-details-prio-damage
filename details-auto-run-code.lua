c34a081fd026a048378d2c09dcc0501013a7099e
Mikael Hedberg <riskfreeloss@users.noreply.github.com>
2025-08-17 17:35:12 +0200

    TWW S3 update

--- details-auto-run-code.lua
+++ details-auto-run-code.lua
@@ -5,45 +5,17 @@
 -- Keeping old seasons data in case the dungeons are ever brought back
 -- if you have other auto run code, don't overwrite when copy-pasting this
 
+-- Add / change only here
 Details.vid_npcs_names = {
     -- testing
-    ["Cleave Training Dummy"]=true,
+    --["Cleave Training Dummy"]=true,
     
-    -- Theater of Pain
-    ["Raging Bloodhorn"]=true,
-    ["Rancid Gasbag"]=true,
-    ["Portal Guardian"]=true,    
-    ["Putrid Butcher"]=true,
-    ["Diseased Horror"]=true,
-    ["Nefarious Darkspeaker"]=true,
-    ["Dokigg the Brutalizer"]=true,
-    ["Nekthara the Mangler"]=true,
-    ["Harugia the Bloodthirsty"]=true,
-    ["Heavin the Breaker"]=true,
-    ["Ancient Captain"]=true,
-    ["Rek the Hardened"]=true,
-    ["Advent Nevermore"]=true,
-    
-    -- mechagon: workshop
-    ["Waste Processing Unit"]=true,
-    ["Spider Tank"]=true,
-    ["Defense Bot Mk III"]=true,
-    
-    -- Motherlode
-    ["Mechanized Peacekeeper"]=true,
-    ["Addled Thug"]=true,
-    ["Azerite Extractor"]=true,
-    ["Taskmaster Askari"]=true,
-    ["Earthrager"]=true,
-    ["Venture Co. Mastermind"]=true,
-    ["Venture Co. War Machine"]=true,
-    
-    -- Cinderbrew Meadery
-    ["Hired Muscle"]=true,
-    ["Chef Chewie"]=true,
-    ["Careless Hopgoblin"]=true,
-    ["Venture Co. Honey Harvester"]=true,
-    ["Venture Co. Honey Harvester"]=true,
+    -- Operation: Floodgate
+    ["Darkfuse Jumpstarter"]=true,
+    ["Venture Co. Architect"]=true,
+    ["Shreddinator 3000"]=true,
+    ["Darkfuse Demolitionist"]=true,
+    ["Darkfuse Bloodwarper"]=true,
     
     -- Priory of the Sacred Flame
     ["Guard Captain Suleyman"]=true,
@@ -53,29 +25,117 @@ Details.vid_npcs_names = {
     ["Sergeant Shaynemail"]=true,
     ["Taener Duelmal"]=true,
     ["Ardent Paladin"]=true,
-    ["Lightspawn"]=true,
     ["Sir Braunpyke"]=true,
     
+    -- Ara-Kara
+    ["Trilling Attendant"]=true,
+    ["Ixin"]=true,
+    ["Nakt"]=true,
+    ["Atik"]=true,
+    ["Hulking Bloodguard"]=true,
+    ["Sentry Stagshell"]=true,
+    ["Blood Overseer"]=true,
+    ["Nerubian Hauler"]=true,
+    
+    -- The Dawnbreaker
+    ["Nightfall Ritualist"]=true,
+    ["Nightfall Commander"]=true,
+    ["Sureki Webmage"]=true,
+    ["Ixkreten The Unbreakable"]=true,
+    ["Ascendant Viscoxria"]=true,
+    ["Deathscreamer Ikentak"]=true,
+    ["Nightfall Tactician"]=true,
+    ["Sureki Militant"]=true,
+    ["Nightfall Dark Architect"]=true,
+    
+    -- Tazavesh: Gambitr
+    ["Coastwalker Goliath"]=true,
+    ["Invigorating Fish Stick"]=true,
+    ["Wandering Pulsar"]=true,
+    ["Adorned Starseer"]=true,
+    
+    -- Tazavesh: Streets
+    ["Gatewarden Zo'mazz"]=true,
+    ["Armored Overseerk"]=true,
+    ["Portalmancer Zo'honn"]=true,
+    ["Tracker Zo'korss"]=true,
+    ["Enraged Direhorn"]=true,
+    ["Commerce Enforcer"]=true,
+    ["Bazaar Overseer"]=true,
+    ["Commander Zo'far"]=true,
+    
+    -- Halls of Atonement
+    ["Stoneborn Slasher"]=true,
+    ["Shard of Halkias"]=true,
+    ["Stoneborn Reaver"]=true,
+    ["Inquisitor Sigar"]=true,
+    
+    -- Eco-Dome Aldani
+    ["Ravenous Destroyer"]=true,
+    ["Overcharged Sentinel"]=true,
+    ["Wastelander Pactspeaker"]=true,
+    ["Burrowing Creeper"]=true,
+    
+    
+    -- Theater of Pain
+    --["Raging Bloodhorn"]=true,
+    --["Rancid Gasbag"]=true,
+    --["Portal Guardian"]=true,    
+    --["Putrid Butcher"]=true,
+    --["Diseased Horror"]=true,
+    --["Nefarious Darkspeaker"]=true,
+    --["Dokigg the Brutalizer"]=true,
+    --["Nekthara the Mangler"]=true,
+    --["Harugia the Bloodthirsty"]=true,
+    --["Heavin the Breaker"]=true,
+    --["Ancient Captain"]=true,
+    --["Rek the Hardened"]=true,
+    --["Advent Nevermore"]=true,
+    --["Oppressive Banner"]=true,
+    
+    -- mechagon: workshop
+    --["Waste Processing Unit"]=true,
+    --["Spider Tank"]=true,
+    --["Defense Bot Mk III"]=true,
+    
+    -- Motherlode
+    --["Mechanized Peacekeeper"]=true,
+    --["Addled Thug"]=true,
+    --["Azerite Extractor"]=true,
+    --["Taskmaster Askari"]=true,
+    --["Earthrager"]=true,
+    --["Venture Co. Mastermind"]=true,
+    --["Venture Co. War Machine"]=true,
+    
+    -- Cinderbrew Meadery
+    --["Hired Muscle"]=true,
+    --["Chef Chewie"]=true,
+    --["Careless Hopgoblin"]=true,
+    --["Venture Co. Honey Harvester"]=true,
+    
     -- Darkflame Cleft
-    ["Rank Overseer"]=true,
-    ["Wandering Candle"]=true,
-    ["Torchsnarl"]=true,
-    ["Sootsnout"]=true,
-    ["Corridor Creeper"]=true,
+    --["Rank Overseer"]=true,
+    --["Wandering Candle"]=true,
+    --["Torchsnarl"]=true,
+    --["Sootsnout"]=true,
+    --["Corridor Creeper"]=true,
+    --["Ol' Waxbeard"]=true,
+    --["Dynamite Mine Cart"]=true,
+    
     
     -- The Rookery
-    ["Quartermaster Koratite"]=true,
-    ["Coalescing Void Diffuser"]=true,
-    ["Radiating Voidstone"]=true,
-    ["Void Ascendant"]=true,
+    --["Quartermaster Koratite"]=true,
+    --["Coalescing Void Diffuser"]=true,
+    --["Radiating Voidstone"]=true,
+    --["Void Ascendant"]=true,
+    --["Voidrider"]=true,
+    
     
     -- The Rookery
-    ["Shreddinator 3000"]=true,
-    ["Darkfuse Bloodwarper"]=true,
-    ["Darkfuse Jumpstarter"]=true,
-    ["Bubbles"]=true,
-    
-    
+    --["Shreddinator 3000"]=true,
+    --["Darkfuse Bloodwarper"]=true,
+    --["Darkfuse Jumpstarter"]=true,
+    --["Bubbles"]=true,
     
     -- Grim Batol
     -- ["Twilight Destroyer"]=true,
@@ -85,84 +145,63 @@ Details.vid_npcs_names = {
     --["Faceless Corruptor"]=true,
     
     -- Siege of Boralus
-    --["Scrimshaw Enforcer"]=true,
-    --["Irontide Raider"]=true,
+    -- ["Scrimshaw Enforcer"]=true,
+    -- ["Irontide Raider"]=true,
     --["Ashvane Commander"]=true,
     --["Ashvane Cannoneer"]=true,
     --["Bilge Rat Demolisher"]=true,
     
     -- Mists of Tirna Scithe
-    --["Tirnenn Villager"]=true,
+    -- ["Tirnenn Villager"]=true,
     --["Drust Boughbreaker"]=true,
-    --["Mistveil Guardian"]=true,
-    --["Mistveil Stalker"]=true,
-    --["Mistveil Matriarch"]=true,
-    --["Mistveil Nightblossom"]=true,
-    --["Mistveil Gorgegullet"]=true,
-    --["Spineway Staghorn"]=true,
+    -- ["Mistveil Guardian"]=true,
+    -- ["Mistveil Stalker"]=true,
+    -- ["Mistveil Matriarch"]=true,
+    -- ["Mistveil Nightblossom"]=true,
+    -- ["Mistveil Gorgegullet"]=true,
+    -- ["Spineway Staghorn"]=true,
     
     -- Necrotic Wake
-    --["Stitched Vanguard"]=true,
-    --["Zolramus Gatekeeper"]=true,
-    --["Zolramus Necromancer"]=true,
-    --["Skeletal Marauder"]=true,
+    -- ["Stitched Vanguard"]=true,
+    -- ["Zolramus Gatekeeper"]=true,
+    -- ["Zolramus Necromancer"]=true,
+    -- ["Skeletal Marauder"]=true,
     --["Nar'zudah"]=true,
-    --["Skeletal Monstrosity"]=true,
-    --["Kyrian Stitchwerk"]=true,
-    --["Goregrind"]=true,
-    --["Rotspew"]=true,
-    
-    -- Ara-Kara
-    --["Trilling Attendant"]=true,
-    --["Ixin"]=true,
-    --["Nakt"]=true,
-    --["Atik"]=true,
-    --["Hulking Bloodguard"]=true,
-    --["Sentry Stagshell"]=true,
-    --["Blood Overseer"]=true,
-    --["Nerubian Hauler"]=true,
+    -- ["Skeletal Monstrosity"]=true,
+    -- ["Kyrian Stitchwerk"]=true,
+    -- ["Goregrind"]=true,
+    -- ["Rotspew"]=true,
     
     -- City of Threads
-    --["Royal Swarmguard"]=true,
+    -- ["Royal Swarmguard"]=true,
     --["Herald of Ansurek"]=true,
-    --["Eye of the Queen"]=true,
-    --["Royal Venomshell"]=true,
-    --["Unstable Test Subject"]=true,
-    --["Hulking Bloodguard"]=true,
-    --["Hulking Warshell"]=true,
-    --["Elder Shadeweaver"]=true,
-    
-    -- The Dawnbreaker
-    --["Nightfall Ritualist"]=true,
-    --["Nightfall Commander"]=true,
-    --["Sureki Webmage"]=true,
-    --["Ixkreten The Unbreakable"]=true,
-    --["Ascendant Viscoxria"]=true,
-    --["Deathscreamer Ikentak"]=true,
-    --["Nightfall Tactician"]=true,
-    --["Sureki Militant"]=true,
-    --["Nightfall Dark Architect"]=true,
+    -- ["Eye of the Queen"]=true,
+    -- ["Royal Venomshell"]=true,
+    -- ["Unstable Test Subject"]=true,
+    -- ["Hulking Bloodguard"]=true,
+    -- ["Hulking Warshell"]=true,
+    -- ["Elder Shadeweaver"]=true,
     
     -- Stonevault
-    --["Earth Infused Golem"]=true,
-    --["Void Bound Despoiler"]=true,
-    --["Rock Smasher"]=true,
-    --["Forge Loader"]=true,
+    -- ["Earth Infused Golem"]=true,
+    -- ["Void Bound Despoiler"]=true,
+    -- ["Rock Smasher"]=true,
+    -- ["Forge Loader"]=true,
     
     -- Dawn of the Infinite
-    --["Epoch Ripper"]=true,
-    --["Valow, Timesworn Keeper"]=true,
-    --["Spurlok, Timesworn Sentinel"]=true,
-    --["Lerai, Timesworn Maiden"]=true,
-    --["Alliance Archer"]=true,
-    --["Horde Axe Thrower"]=true,
-    --["Anduin Lothar"]=true,
-    --["Alliance Destroyer"]=true,
-    --["Faceless Watcher"]=true,
-    --["Tainted Sentry"]=true,
-    --["Risen Dragon"]=true,
-    --["Coalesced Time"]=true,
-    --["Timestream Anomaly"]=true,
+    -- ["Epoch Ripper"]=true,
+    -- ["Valow, Timesworn Keeper"]=true,
+    -- ["Spurlok, Timesworn Sentinel"]=true,
+    -- ["Lerai, Timesworn Maiden"]=true,
+    -- ["Alliance Archer"]=true,
+    -- ["Horde Axe Thrower"]=true,
+    -- ["Anduin Lothar"]=true,
+    -- ["Alliance Destroyer"]=true,
+    -- ["Faceless Watcher"]=true,
+    -- ["Tainted Sentry"]=true,
+    -- ["Risen Dragon"]=true,
+    -- ["Coalesced Time"]=true,
+    -- ["Timestream Anomaly"]=true,
     
     -- Ruby Life Pools
     --["Primal Juggernaut"]=true,
